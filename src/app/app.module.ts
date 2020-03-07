228f59236e3fdc1ee64a4e97fc82461e058ae742
Max Alva <alva85@gmail.com>
2020-03-07 11:10:36 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
+
+
+
 
 @NgModule({
   declarations: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  wsUrl: 'https://sockets-graficas-angular.maxalva.com'
 };
